20a51548215b2e47b9994da7f8e879691c06e878
Shivam Sharma <shivams9575@gmail.com>
2025-10-12 00:32:24 +0530

    Trigger SonarCloud CI with JaCoCo and Checkstyle

--- src/main/java/com/andela/british_time_in_words/controller/TimeController.java
+++ src/main/java/com/andela/british_time_in_words/controller/TimeController.java
@@ -4,7 +4,12 @@ import com.andela.british_time_in_words.service.TimeConversionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @Slf4j
 @RestController
